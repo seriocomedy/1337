15baa73a92f67a2cc9f9b5d8f016346ffbe735ef
Carver Harrison <pcarverb@hotmail.com>
2016-11-01 10:44:36 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -15,7 +15,7 @@ jQuery(function($, undefined) {
     }, {
         greetings: '1337 Version 1.0',
         name: 'terminal',
-        height: window.screen.height,
+        height: window.screen.availHeight,
         prompt: '> '
     });
 });
